93da240c719f0a5f871f4c6cc90bd2314258efcd
Jan Wilhelm <wilhelm.jan92@gmail.com>
2022-07-17 17:54:18 +0200

    add display books function

--- index.js
+++ index.js
@@ -5,3 +5,40 @@ class Book {
     this.isbn = isbn;
   }
 }
+
+class UI {
+  static displayBooks() {
+    const StoredBooks = [
+      {
+        title: "Headstrong",
+        author: "Dave Asprey",
+        isbn: "9780062652416",
+      },
+      {
+        title: "Boundless",
+        author: "Ben Greenfield",
+        isbn: "1628603976",
+      },
+      {
+        title: "Code of the extraordinary mind",
+        author: "Vishen Lakhiani",
+        isbn: "1623367085",
+      },
+    ];
+
+    const books = StoredBooks;
+
+    books.forEach((book) => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector("#book-list");
+    const row = document.createElement("tr");
+
+    row.innerHTML = `<td>${book.title}</td><td>${book.author}</td><td>${book.title}</td><td><a href="#" btn btn-danger><i class="fa-solid fa-trash"></i></a></td>`;
+
+    list.appendChild(row);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
