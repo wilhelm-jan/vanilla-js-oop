92b872dd668f63989fa0e72caff23483f38bf585
Jan Wilhelm <wilhelm.jan92@gmail.com>
2022-07-17 17:57:56 +0200

    add submit function

--- index.js
+++ index.js
@@ -42,3 +42,14 @@ class UI {
 }
 
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
+
+  const book = new Book(title, author, isbn);
+
+  console.log(book);
+});
